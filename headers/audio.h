ed7923911d914ff8f5b2a9f9e0f5eae2761f530a
vzze <lolo32778@gmail.com>
2023-07-11 20:00:36 +0300

    wrap c wrapper

--- headers/audio.h
+++ headers/audio.h
@@ -9,42 +9,39 @@
 
 #ifdef __cplusplus
 extern "C" {
+    namespace vmp {
 #endif
 
 #ifndef __cplusplus
 #include <stdbool.h>
-#include <wchar.h>
 #endif
-
-struct ma_engine;
-struct ma_sound;
-
+        struct ma_engine;
+        struct ma_sound;
 #ifndef __cplusplus
-typedef struct ma_engine engine;
-typedef struct ma_sound sound;
+        typedef struct ma_engine engine;
+        typedef struct ma_sound sound;
 #else
-using engine = struct ma_engine;
-using sound  = struct ma_sound;
+        using engine = struct ma_engine;
+        using sound  = struct ma_sound;
 #endif
+        bool engine_init(engine **);
 
-bool engine_init(engine **);
+        bool sound_init(engine *, const char *, sound **);
 
-bool sound_init(engine *, const char *, sound **);
+        void sound_play       (sound *       );
+        void sound_stop       (sound *       );
+        void sound_rewind     (sound *       );
+        void sound_set_volume (sound *, float);
+        bool sound_is_playing (sound *       );
+        void sound_set_looping(sound *, bool );
+        bool sound_is_looping (sound *       );
+        bool sound_is_at_end  (sound *       );
 
-void sound_play       (sound *       );
-void sound_stop       (sound *       );
-void sound_rewind     (sound *       );
-void sound_set_volume (sound *, float);
-bool sound_is_playing (sound *       );
-void sound_set_looping(sound *, bool );
-bool sound_is_looping (sound *       );
-bool sound_is_at_end  (sound *       );
-
-void sound_free(sound *);
-void engine_free(engine *);
-
+        void sound_free(sound *);
+        void engine_free(engine *);
 #ifdef __cplusplus
-} // extern "C"
+    }
+}
 #endif
 
 #endif
